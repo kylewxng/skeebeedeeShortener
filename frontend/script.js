d5224b0d701006de681cb6b420000394981b5f67
kylewxng <kylewxng06@gmail.com>
2025-03-03 18:50:57 -0800

    login

---
+++ frontend/script.js
@@ -0,0 +1,7 @@
+function toggleForm() {
+    const signupForm = document.getElementById("signupContainer");
+    const loginForm = document.getElementById("loginContainer");
+
+    signupForm.classList.toggle("active");
+    loginForm.classList.toggle("active");
+}
