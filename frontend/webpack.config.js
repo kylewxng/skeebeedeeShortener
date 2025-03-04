5bec63556ddd9b491082a28b771f115555c19f09
Bradley Co <bradco626@gmail.com>
2025-03-03 20:42:24 -0800

    firebase setup in progress

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,11 +1,19 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin'); 
 
 module.exports = {
     mode: 'development',
     devtool: 'eval-source-map',
-    entry: './firebase.js',
+    entry: './firebase.js',  
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
-    }
+    },
+    watch: true,  
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './login.html',  
+            filename: 'index.html'  
+        })
+    ]
 };
