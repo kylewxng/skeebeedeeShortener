79478f5cfec9a5614c3ebb3ebc885fbbc5fd360d
Bradley Co <bradco626@gmail.com>
2025-03-03 19:41:06 -0800

    set up webpack

--- frontend/firebaseConfig.js
+++ frontend/firebaseConfig.js
@@ -1,4 +1,6 @@
 import { initializeApp } from 'firebase/app';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore'
 
 const firebaseApp = initializeApp({
     apiKey: "AIzaSyDjr6ks75nLF4KX1uUIkBnySWxz0wVqfAs",
@@ -8,5 +10,16 @@ const firebaseApp = initializeApp({
     messagingSenderId: "170765092469",
     appId: "1:170765092469:web:275d6f6d913d550effda3c",
     measurementId: "G-HEPYKNNCV1"
-  });
-  
+}); // This must be first before any service function
+
+const auth = getAuth(firebaseApp);
+const db = getFirestore(firebaseApp);
+
+//Detect auth state
+onAuthStateChanged(auth, user => {
+    if(user != null){
+        console.log('logged in')
+    }else{
+        console.log('no user')
+    }
+})
