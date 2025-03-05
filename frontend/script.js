6941236cea3603fcc39f4d22c1770ae2cdc2516f
bradwco <bradco626@gmail.com>
2025-03-04 16:30:47 -0800

    Working authentication with email verification

--- frontend/script.js
+++ frontend/script.js
@@ -6,12 +6,3 @@ function toggleForm() {
     loginForm.classList.toggle("active");
 }
 
-const buttons = document.querySelectorAll(".submitButton");
-
-buttons.forEach(button => {
-    button.addEventListener('click', function(event) {
-        event.preventDefault();
-
-        window.location.href="index.html";
-    })
-});
